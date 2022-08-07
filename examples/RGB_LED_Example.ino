fd2a976325a81a956fe0078bbbc437b655ba01a3
flouis01 <48184466+flouis01@users.noreply.github.com>
2022-08-07 23:42:42 +0200

    Update RGB_LED_Example.ino

--- examples/RGB_LED_Example.ino
+++ examples/RGB_LED_Example.ino
@@ -3,18 +3,21 @@
     RGB_LED_Example
   Discription:
     This code uses the RGB_LED library to make the a RGB-LED cycle colors.
+  Functions:
+    RGB_LED [name]; creates a new RGB_LED object with the specified name. 
+      .setup(int RED, int Green, int BLUE) sets the to the colors corresponding pins.
+      .mono(int volume) sets the LED to a grayscale value.
+      .fadeMono(int volume, int time) makes the LED fade to a grayscale value over the specified time. 
+      .RGB(int RED, int Green, int BLUE, int time) sets the LED to a RGB value.
+      .fadeRGB(int RED, int Green, int BLUE, int time) makes the LED fade to a RGB value over the specified time. 
 */
 
 #include <RGB_LED.h>
 
-RGB_LED LED;
+RGB_LED LED; 
 
 void setup(){
-  LED.setup(6, 5, 3);
-  LED.Mono(255);
-  delay(1000);
-  LED.Mono(0);
-  delay(1000);
+  LED.setup(6, 5, 3); 
 };
 
 void loop(){
@@ -42,8 +45,8 @@ void loop(){
   delay(1000);
   LED.RGB(255, 0, 250);
   delay(1000);
-  LED.Mono(255);
+  LED.mono(255);
   delay(1000);
-  LED.Mono(0);
+  LED.mono(0);
   delay(1000);
 };
